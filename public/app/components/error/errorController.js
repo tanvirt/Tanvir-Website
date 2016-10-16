4d3e95415fba207ec628533c12fead9c5e876050
tanvirt <tanvirt@ufl.edu>
2016-10-16 00:09:55 -0500

    Added new pictures and heading texts

--- public/app/components/error/errorController.js
+++ public/app/components/error/errorController.js
@@ -3,7 +3,7 @@ angular.module('main').controller('ErrorController', function($scope, $state) {
 	// App header variables
 	$scope.heading = "404";
 	$scope.subheading = "Page not found.";
-	$scope.headingImage = "../../assets/img/splash.jpg";
+	$scope.headingImage = "../../assets/img/post.jpg";
 
 	// Main content starts
 
